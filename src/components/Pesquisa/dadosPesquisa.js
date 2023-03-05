cc53b2a3a950d802e62807284a563ae9d865019d
uiratan <uiratan@gmail.com>
2023-03-05 04:29:55 -0300

    criando componente Pesquisa usando estado de componente

---
+++ src/components/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,10 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', id:1, src: livro},
+    {nome: 'Liderança em UX', id:1, src: livro},
+    {nome: 'JavaScript assertivo', id:2, src: livro},
+    {nome: 'Cangaceiro em JavaScript', id:3, src: livro},
+    {nome: 'Clean Code', id:4, src: livro},
+    {nome: 'Pragmatic Programmer', id:5, src: livro}
+]
--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 import Input from '../Input';
+import { useState } from 'react';
+import { livros } from './dadosPesquisa';
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
     color: #FFF;
     text-align: center;
     padding: 85px 0;
-    height: 270px;
+    height: 470px;
     width: 100%;
 `
 const Titulo = styled.h2`
@@ -20,16 +22,48 @@ const Subtitulo = styled.h3`
     font-weight: 500;
     margin-bottom: 40px;
 `
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
 function Pesquisa() {
+  const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+
+  function fazPesquisa(evento) {
+    const textoDigitado = evento.target.value
+    const resultadoPesquisa = livros.filter( 
+      livro => livro.nome.toUpperCase().includes(textoDigitado.toUpperCase()))
+    setLivrosPesquisados(resultadoPesquisa)
+}
+
   return (
     <PesquisaContainer>
-      <Titulo>Já sabe por onde começar?</Titulo>
-      <Subtitulo>Encontre seu livro em nossa estante</Subtitulo>
-      <Input 
-        placeholder='Escreva sua próxima leitura'
-      />
-    </PesquisaContainer>
+           <Titulo>Já sabe por onde começar?</Titulo>
+           <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+           <Input
+               placeholder="Escreva sua próxima leitura"
+               onChange={evento => fazPesquisa(evento)}
+           />
+           { livrosPesquisados.map( livro => (
+               <Resultado>
+                   <img src={livro.src}/>
+                   <p>{livro.nome}</p>
+               </Resultado>
+           ) ) }
+       </PesquisaContainer>
   )
 }
 
