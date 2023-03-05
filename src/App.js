61d300313d5b8f22ad195557a052f87a2ce3cb8d
uiratan <uiratan@gmail.com>
2023-03-05 00:50:19 -0300

    css da app retirado e configurado GlobalStyle

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(90deg, #002F52 35%, #326589 165%);
-  
-
-  li {
-    list-style: none;
-  }
 `
 
 function App() {
